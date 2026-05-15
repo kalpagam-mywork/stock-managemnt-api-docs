c3f04b578b5d6a05061e15d52acba1576840b8ca
kalpagam-mywork <kalpagamaravinth@gmail.com>
2026-05-15 14:51:27 +0530

    Updated the netlify published URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://stock-management-docs.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kalpagam-mywork', // Usually your GitHub org/user name.
+  projectName: 'stock-managemnt-api-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
